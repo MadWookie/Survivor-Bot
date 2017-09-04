344087375c1b2c961432cf6e5f83830756646f62
MadWookie <william.haddix@yahoo.com>
2017-09-03 23:02:44 -0400

    Fixed typo

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -274,7 +274,7 @@ class Pokemon(Menus):
         found = {k: v for k, v in userdata['pokemon'].items() if v}
         found_sorted = sorted(found)
         found_names = [self.poke_info[num]['name'] for num in found_sorted]
-        header = f'**{player_name}**,\nSelect Pokemon to sell.\nNormal = 100\ua750\nLegendary = 600\ua750\Mythical = 1000\ua750'
+        header = f'**{player_name}**,\nSelect Pokemon to sell.\nNormal = 100\ua750\nLegendary = 600\ua750\nMythical = 1000\ua750'
         options = ['**{}.** {[name]}{}{}'.format(mon, self.poke_info[mon], star if self.poke_info[mon]['legendary'] else glowing_star if self.poke_info[mon]['mythical'] else '', f' *x{found[mon]}*' if found[mon] > 1 else '') for mon in found_sorted]
         if not options:
             await ctx.send("You don't have any pokemon to sell.", delete_after=60)
