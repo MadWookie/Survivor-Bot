f8453f2fd23a093397a6c4dadc01199fdd0a5b8b
MadWookie <william.haddix@yahoo.com>
2017-09-12 14:58:36 -0400

    Changed variable names from xp to exp

--- cogs/exp.py
+++ cogs/exp.py
@@ -9,11 +9,11 @@ import discord
 from cogs.menus import Menus, ARROWS, CANCEL
 from utils import checks
 
-xp_cooldown = 60
-xp_min_amount = 15
-xp_max_amount = 25
-# xp_min_amount = 30 #Double XP
-# xp_max_amount = 50 #Double XP
+exp_cooldown = 60
+exp_min_amount = 15
+exp_max_amount = 25
+# exp_min_amount = 30 #Double EXP
+# exp_max_amount = 50 #Double EXP
 
 
 def level_req(level):
@@ -37,7 +37,7 @@ class Exp(Menus):
         if member.guild.id not in self.cooldowns:
             self.cooldowns[member.guild.id] = {}
         if member.id in self.cooldowns[member.guild.id]:
-            if time.time() - self.cooldowns[member.guild.id][member.id] < xp_cooldown:
+            if time.time() - self.cooldowns[member.guild.id][member.id] < exp_cooldown:
                 return False
         return True
 
@@ -55,7 +55,7 @@ class Exp(Menus):
         if self.check_cd(message.author):
             prestiged = False
             leveled = False
-            add_xp = randint(xp_min_amount, xp_max_amount)
+            add_exp = randint(exp_min_amount, exp_max_amount)
             async with self.bot.db_pool.acquire() as con:
                 for wait in range(3):  # try updating XP 3 times.
                     try:
@@ -64,12 +64,12 @@ class Exp(Menus):
                                 SELECT exp, level, prestige FROM experience WHERE user_id = $1 AND guild_id = $2
                                 ''', uid, gid) or {'exp': 0, 'level': 0, 'prestige': 0}
                             self.cooldowns[gid][uid] = time.time()
-                            xp = rec['exp'] + add_xp
+                            exp = rec['exp'] + add_exp
                             level = rec['level']
                             prestige = rec['prestige']
                             to_next_level = level_req(level + 1)
-                            if xp >= to_next_level:
-                                xp -= to_next_level
+                            if exp >= to_next_level:
+                                exp -= to_next_level
                                 level += 1
                                 if level == 30:
                                     level = 0
@@ -81,7 +81,7 @@ class Exp(Menus):
                                 INSERT INTO experience (guild_id, user_id, exp, level, prestige) VALUES ($1, $2, $3, $4, $5)
                                 ON CONFLICT (guild_id, user_id) DO
                                 UPDATE SET exp = $3, level = $4, prestige = $5
-                                ''', gid, uid, xp, level, prestige)
+                                ''', gid, uid, exp, level, prestige)
                     except asyncpg.SerializationError:
                         prestiged = False  # in case fails 3 times
                         leveled = False
@@ -111,15 +111,15 @@ class Exp(Menus):
         gid = ctx.guild.id
         rec = await ctx.con.fetchrow('''
             SELECT exp, level, prestige FROM experience WHERE user_id = $1 AND guild_id = $2
-            ''', uid, gid) or {'xp': 0, 'level': 0, 'prestige': 0}
-        xp, level, prestige = rec['xp'], rec['level'], rec['prestige']
+            ''', uid, gid) or {'exp': 0, 'level': 0, 'prestige': 0}
+        exp, level, prestige = rec['exp'], rec['level'], rec['prestige']
         to_next_level = level_req(level + 1)
         thumbnail = f'http://unitedsurvivorsgaming.com/P{prestige}.png'
         embed = discord.Embed(colour=0xffffff)
         embed.set_author(name=name, icon_url=user.avatar_url)
         embed.set_thumbnail(url=thumbnail)
         embed.add_field(name='LEVEL', value=level)
-        embed.add_field(name='XP', value=f'{xp}/{to_next_level}')
+        embed.add_field(name='EXP', value=f'{exp}/{to_next_level}')
         await ctx.send(embed=embed)
 
 ###################
@@ -132,17 +132,16 @@ class Exp(Menus):
     @commands.command()
     async def rank(self, ctx):
         ordered = await ctx.con.fetch('''
-            SELECT * FROM experience WHERE guild_id = $1 ORDER BY prestige DESC, level DESC, xp DESC
+            SELECT * FROM experience WHERE guild_id = $1 ORDER BY prestige DESC, level DESC, exp DESC
             ''', ctx.guild.id)
         options = [[{'name': 'Rank', 'value': ''}, {'name': 'User', 'value': ''}]]
         on_cur_page = 0
         ind = 1
-        for xp in ordered:
+        for exp in ordered:
             if on_cur_page == 10:
                 options.append([{'name': 'Rank', 'value': ''}, {'name': 'User', 'value': ''}])
                 on_cur_page = 0
-            uid, stats = pair
-            member = ctx.guild.get_member(uid)
+            member = ctx.guild.get_member(exp['user_id'])
             if member is None:
                 continue
             if options[-1][0]['value']:
